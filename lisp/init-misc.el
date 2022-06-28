ce2dd8cb4d6ea20379cd4a2bab2420c4f18f3188
Arto Young <arto.young@outlook.com>
2022-06-29 00:07:27 +0800

    Add some miscellaneous configuration
    1. no tab for indent by default
    2. save file visit place
    3. scroll settings

---
+++ lisp/init-misc.el
@@ -0,0 +1,18 @@
+(setq indent-tabs-mode nil)
+(save-place-mode 1)
+
+;;scroll configuration
+(setq scroll-up-aggressively 0.1
+      scroll-down-aggressively 0.1
+      scroll-preserve-screen-position t
+      scroll-margin 3
+      scroll-step 1)
+
+(defadvice scroll-up-command (before scroll-up-less-line activate compile)
+  (if (not arg) (setq arg 15)))
+(defadvice scroll-down-command (before scroll-down-less-line activate compile)
+  (if (not arg) (setq arg 15)))
+
+(global-set-key (kbd "M-k") #'scroll-down-line)
+(global-set-key (kbd "M-j") #'scroll-up-line)
+(global-set-key (kbd "C-M-j") #'join-line)
