e1b708945ff0cca469c31bf1a74880613ade3784
Arto Young <arto.young@outlook.com>
2022-06-30 07:15:03 +0800

    Put fullscreen parameter into default-frame-alist

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,11 +2,6 @@
 (setq frame-inhibit-implied-resize t
       frame-resize-pixelwise t)
 
-(unless (eq (frame-parameter nil 'fullscreen) 'maximized)
-  (set-frame-parameter nil 'fullscreen 'maximized)
-  (set-frame-parameter nil 'left 0)
-  (set-frame-parameter nil 'top 0))
-
 (set-frame-font
  (font-spec :name "Sarasa Mono SC"
 	    :size 14.0
