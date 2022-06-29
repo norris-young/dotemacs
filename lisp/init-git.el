730a34f5bcd6d2e18c069abc7c2f51377134fd1e
Arto Young <arto.young@outlook.com>
2022-06-30 06:22:38 +0800

    Limit magit log in 1 year for better performance

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,3 +1,5 @@
 (use-package magit
   :config
+  (put 'magit-log-mode 'magit-log-default-arguments
+       '("--graph" "-n256" "--decorate" "--since=1.year"))
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
