bb9b1c2026854300d65d3b42a4d13f044d596f7a
Arto Young <arto.young@outlook.com>
2022-07-02 04:44:31 +0800

    Use traditional function to display magit buffer
    Fullframe function seems to have conflict with shackle package.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,4 +2,4 @@
   :config
   (put 'magit-log-mode 'magit-log-default-arguments
        '("--graph" "-n256" "--decorate" "--since=1.year"))
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+  (setq magit-display-buffer-function #'magit-display-buffer-traditional))
