02177b3548bb23b692fb940be96473cfc22fe640
Arto Young <arto.young@outlook.com>
2023-02-09 23:33:46 +0800

    Add citre integrated with lsp-bridge

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -15,5 +15,16 @@
               (if (string-match "linux" (buffer-file-name))
                   (c-set-style "linux-kernel")
                 (c-set-style "linux-user")))))
+(use-package citre
+  :commands (citre-update-tags-file citre-update-this-tags-file citre-edit-tags-file-recipe citre-create-tags-file citre-global-create-database citre-global-update-database)
+  :hook (prog-mode . citre-auto-enable-citre-mode)
+  :custom
+  (citre-auto-enable-citre-mode-modes '(prog-mode))
+  (citre-use-project-root-when-creating-tags t)
+  (citre-gtags-args '("--compact"))
+  :config
+  (with-eval-after-load 'cc-mode (require 'citre-lang-c))
+  (with-eval-after-load 'dired (require 'citre-lang-fileref))
+  (with-eval-after-load 'verilog-mode (require 'citre-lang-verilog)))
 
 (provide 'init-c)
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -3,8 +3,51 @@
   (yas-global-mode 1))
 
 (use-package lsp-bridge
+  :custom
+  (acm-enable-citre t)
+  (lsp-bridge-find-def-fallback-function #'my-after-lsp-find-def-failure)
+  (lsp-bridge-find-ref-fallback-function #'my-after-lsp-find-ref-failure)
   :config
   (global-lsp-bridge-mode)
   (setq lsp-bridge-c-lsp-server "ccls"))
+  (defun my-find-def ()
+    (interactive)
+    (xref-push-marker-stack (point-marker))
+    (if (eq major-mode 'emacs-lisp-mode)
+        (let ((symb (symbol-at-point)))
+          (if (and symb (not (fboundp symb)))
+              (find-variable symb)
+            (find-function symb))))
+      (lsp-bridge-find-def))
+
+  (defun my-after-lsp-find-def-failure (position)
+    (deactivate-mark t)
+    (if citre-mode
+        (let ((line (1+ (nth 1 position)))
+              (col (nth 3 position)))
+          (message "lsp-bridge find no reference try citre line: %d col: %d" line col)
+          (goto-line line)
+          (goto-char (+ (line-beginning-position) col))
+          (citre-jump))))
+
+  (defun my-find-ref ()
+    (interactive)
+    (if (eq major-mode 'emacs-lisp-mode)
+        (let ((identifier (xref-backend-identifier-at-point 'elisp)))
+          (xref--find-xrefs identifier 'references identifier nil))
+      (progn
+        (xref-push-marker-stack (point-marker))
+        (lsp-bridge-find-references))))
+
+  (defun my-after-lsp-find-ref-failure (position)
+    (deactivate-mark t)
+    (if citre-mode
+        (let ((line (1+ (nth 1 position)))
+              (col (nth 3 position)))
+          (message "lsp-bridge find no reference try citre line: %d col: %d" line col)
+          (goto-line line)
+          (goto-char (+ (line-beginning-position) col))
+          (citre-jump-to-reference))))
+  )
 
 (provide 'init-lsp)
