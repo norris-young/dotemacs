a7685539b833d1f244e9bf41991d741130b18eb2
Arto Young <arto.young@outlook.com>
2023-02-11 13:05:35 +0800

    Update magit configuration
    1. Stop using fullframe display function which behaviour is abnormal somtimes;
    2. Add default argument "--since=1.year" for magit log.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -3,7 +3,9 @@
         ("s" . magit-status)
         ("b" . magit-blame-addition)
         ("q" . magit-blame-quit))
-  :custom
-  (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
+  :config
+  (transient-replace-suffix 'magit-log 'magit-log:--since
+    '(magit-log:--since :init-value (lambda (obj) (oset obj value "1.year"))))
+  )
 
 (provide 'init-git)
