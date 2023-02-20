8e2aef24d3f5caeb385ab4ed88559e26bdef4d69
Arto Young <arto.young@outlook.com>
2023-02-20 12:53:45 +0800

    Make edebug more usable

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -28,4 +28,32 @@
       (package-subdirs-recurse #'my-collect-package-generated-autoloads path)))
   )
 
+(use-package edebug
+  :bind (:map edebug-mode-map
+         ("A" . 'my-elisp-add-to-watch))
+  :config
+  ;; Simple watch window from emacswiki @https://www.emacswiki.org/emacs/SourceLevelDebugger
+  (defun my-elisp-add-to-watch (&optional region-start region-end)
+    "Add the current variable to the *EDebug* window"
+    (interactive "r")
+    (let ((statement
+             (if (and region-start region-end (use-region-p))
+               (buffer-substring region-start region-end)
+             (symbol-name (elisp--current-symbol)))))
+      ;; open eval buffer
+      (edebug-visit-eval-list)
+      ;; jump to the end of it and add a newline
+      (goto-char (point-max))
+      (newline)
+      ;; insert the variable
+      (insert statement)
+      ;; update the list
+      (edebug-update-eval-list)
+      ;; jump back to where we were
+      (edebug-where)))
+  )
+
+(use-package edebug-x
+  :hook (edebug-mode . edebug-x-mode))
+
 (provide 'init-lisp)
