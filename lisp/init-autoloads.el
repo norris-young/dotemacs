3564713fe482cf38b344f90474811577eef5a123
Arto Young <arto.young@outlook.com>
2023-04-22 11:55:48 +0800

    Auto generate autoloads if there is no directory of autoloads

--- lisp/init-autoloads.el
+++ lisp/init-autoloads.el
@@ -1,6 +1,14 @@
 (defvar my-autoloads-dir
   (concat (file-name-as-directory my-lisp-dir) "autoloads"))
 
+(if (not (file-directory-p my-autoloads-dir))
+    (progn
+      (make-directory my-autoloads-dir)
+      (add-hook 'after-init-hook (lambda ()
+                                   (package-subdirs-recurse
+                                    #'my-collect-package-generated-autoloads
+                                    my-packages-dir)))))
+
 (add-to-list 'load-path my-autoloads-dir)
 
 (cl-dolist (alf (directory-files my-autoloads-dir nil "^[^.].*"))
