035a5ef68931ddc6f08031e9ade6653698246820
Arto Young <arto.young@outlook.com>
2024-01-06 18:37:59 +0800

    Add package better-jumper
    Use better-jumper to replace the xref marker mechanism.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -5,17 +5,31 @@
   (define-key yas-minor-mode-map (kbd "TAB") nil)
   (define-key yas-minor-mode-map (kbd "C-<tab>") yas-maybe-expand))
 
-(use-package xref
-  :autoload
-  xref-push-marker-stack
-  xref--find-xrefs)
+;; (use-package xref
+;;   :autoload
+;;   xref-backend-identifier-at-point
+;;   xref--find-xrefs)
+
+(use-package better-jumper
+  :bind (:map
+         my-function-map
+         ("f" . better-jumper-jump-forward)
+         ("b" . better-jumper-jump-backward))
+  :custom
+  (better-jumper-add-jump-behavior 'replace)
+  :hook (after-init . better-jumper-mode)
+  :config
+  (advice-add #'switch-to-buffer :before
+              (lambda (&rest r)
+                (better-jumper-set-jump)))
+  (advice-add #'counsel-jump-in-buffer :before #'better-jumper-set-jump)
+  )
 
 (use-package lsp-bridge
   :bind (:map
          my-function-map
          ("d" . my-find-def)
          ("r" . my-find-ref)
-         ("b" . xref-go-back)
          :map
          lsp-bridge-ref-mode-map
          ("d" . lsp-bridge-ref-open-file))
@@ -33,19 +47,34 @@
 
   (add-to-list 'lsp-bridge-default-mode-hooks 'rust-ts-mode-hook)
 
-  (defun my-find-def ()
-    (interactive)
-    (xref-push-marker-stack (point-marker))
-    (if (or (eq major-mode 'emacs-lisp-mode)
-            (eq major-mode 'lisp-interaction-mode))
-        (let ((symb (symbol-at-point)))
-          (if (and symb (not (fboundp symb)))
-              (find-variable symb)
-            (find-function symb)))
-      (if lsp-bridge-mode
-          (lsp-bridge-find-def)
-        (if citre-mode
-            (citre-jump)))))
+  (with-eval-after-load 'better-jumper
+    (defun my-find-def ()
+      (interactive)
+      (better-jumper-set-jump)
+      (if (or (eq major-mode 'emacs-lisp-mode)
+              (eq major-mode 'lisp-interaction-mode))
+          (call-interactively #'xref-find-definitions)
+          ;; (let ((symb (symbol-at-point)))
+          ;;   (if (and symb (not (fboundp symb)))
+          ;;       (find-variable symb)
+          ;;     (find-function symb)))
+        (if lsp-bridge-mode
+            (lsp-bridge-find-def)
+          (if citre-mode
+              (citre-jump)))))
+
+    (defun my-find-ref ()
+      (interactive)
+      (better-jumper-set-jump)
+      (if (or (eq major-mode 'emacs-lisp-mode)
+              (eq major-mode 'lisp-interaction-mode))
+          (call-interactively #'xref-find-references)
+          ;; (let ((identifier (xref-backend-identifier-at-point 'elisp)))
+          ;;   (xref--find-xrefs identifier 'references identifier nil))
+        (if lsp-bridge-mode
+            (lsp-bridge-find-references)
+          (if citre-mode
+              (call-interactively #'citre-jump-to-reference))))))
 
   (defun my-after-lsp-find-def-failure (position)
     (deactivate-mark t)
@@ -56,19 +85,6 @@
           (forward-line line)
           (goto-char (+ (line-beginning-position) col))
           (citre-jump))))
-
-  (defun my-find-ref ()
-    (interactive)
-    (if (or (eq major-mode 'emacs-lisp-mode)
-            (eq major-mode 'lisp-interaction-mode))
-        (let ((identifier (xref-backend-identifier-at-point 'elisp)))
-          (xref--find-xrefs identifier 'references identifier nil))
-      (if lsp-bridge-mode
-          (progn
-            (xref-push-marker-stack (point-marker))
-            (lsp-bridge-find-references))
-        (if citre-mode
-            (call-interactively #'citre-jump-to-reference)))))
 
   (defun my-after-lsp-find-ref-failure (position)
     (deactivate-mark t)
