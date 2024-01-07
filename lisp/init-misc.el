2b019b7ad69cee0c58514e56128a019c59b530f0
Arto Young <arto.young@outlook.com>
2024-01-07 23:08:47 +0800

    Delete unused smartparens bindings

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -11,11 +11,6 @@
   )
 
 (use-package smartparens
-  :bind (("M--" . sp-unwrap-sexp)
-         ("M-[" . sp-backward-slurp-sexp)
-         ("M-]" . sp-forward-slurp-sexp)
-         ("M-{" . sp-backward-barf-sexp)
-         ("M-}" . sp-forward-barf-sexp))
   :custom (sp-c-modes '(c-mode c++-mode c-ts-mode c++-ts-mode))
   :hook (after-init . smartparens-global-mode)
   :config
