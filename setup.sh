0b505e4b0a7ae2d8ab8db9045774dde0828d8636
Arto Young <arto.young@outlook.com>
2025-01-30 19:13:24 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,34 @@
 #! /usr/bin/env bash
 
+# install dependencys
+os=$(cat /etc/os-release | grep '^NAME=')
+if [[ ${os} != "NAME=\"Arch Linux\"" ]]; then
+    echo "we only support Arch linux currently."
+    exit 1
+fi
+
+sudo pacman -S --needed \
+     git \
+     ctags global \
+     python python-pygments \
+     ttf-ubuntu-mono-nerd wqy-microhei ttf-hanazono \
+     clang pyright ruff
+
+# install sarasaTermSCNerd
+font_path=~/.local/share/fonts/
+if [[ ! -d ${font_path} ]]; then
+    mkdir -p ${font_path}
+fi
+wget https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v2.3.1/SarasaTermSCNerd.ttc.tar.gz
+tar xf ./SarasaTermSCNerd.ttc.tar.gz -C ${font_path}
+sudo fc-cache
+rm ./SarasaTermSCNerd.ttc.tar.gz
+
 # setup python venv for lsp bridge
 lsp_bridge_pyenv_path=~/.emacs.d/lsp.bridge.pyenv
+if [[ -d ${lsp_bridge_pyenv_path} ]]; then
+    exit 0
+fi
 python -m venv ${lsp_bridge_pyenv_path}
 source ${lsp_bridge_pyenv_path}/bin/activate
 pip install --upgrade pip
