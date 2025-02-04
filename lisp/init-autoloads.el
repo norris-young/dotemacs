0bb5b6fec17ac3d7dabb9b3f49792ef900cde9c5
Arto Young <arto.young@outlook.com>
2025-02-04 16:48:12 +0800

    Update autoload initialization

--- lisp/init-autoloads.el
+++ lisp/init-autoloads.el
@@ -5,28 +5,31 @@
 (defvar my-autoloads-file
   (expand-file-name "my-autoloads.el" my-lisp-dir))
 
-(add-to-list 'load-path my-lisp-dir)
-(add-to-list 'load-path my-autoloads-dir)
-
-(setq native-comp-async-report-warnings-errors 'silent)
+;; generate autoload for functions(package-subdirs-recurse...) in current init file
 (if (not (file-exists-p my-autoloads-file))
     (loaddefs-generate my-lisp-dir my-autoloads-file))
 (load my-autoloads-file)
 
+;; add load path
+(add-to-list 'load-path my-lisp-dir)
+(add-to-list 'load-path my-autoloads-dir)
 (package-subdirs-recurse #'add-loadpath my-packages-dir)
 (add-loadpath my-sitelisp-dir)
 (package-subdirs-recurse #'add-loadpath my-sitelisp-dir)
 
+;; generate autoload and native compilation for third-party packages and my own defuns
 (if (not (file-directory-p my-autoloads-dir))
     (progn
       (require 'bytecomp)
       (make-directory my-autoloads-dir)
+      (setq native-comp-async-report-warnings-errors 'silent)
       (let ((byte-compile-log-warning-function (lambda (&rest _))))
         (package-subdirs-recurse #'my-collect-package-generated-autoloads
                                  my-packages-dir))
       (native-compile-async my-lisp-dir nil nil "myfun.*")
       ))
 
+;; load autoload for third-party packages
 (cl-dolist (alf (directory-files my-autoloads-dir nil "^[^.].*"))
   (load (file-name-base alf) nil t))
 
@@ -36,6 +39,7 @@
 (setq use-package-expand-minimally nil)
 (setq use-package-enable-imenu-support t)
 
+;; useless for native compilation
 ;; (use-package auto-compile
 ;;   :init
 ;;   (setq load-prefer-newer t)
