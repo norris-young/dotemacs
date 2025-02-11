92fb1c2207372f617f76ed85498394e8dfa2b263
a1ph2 <a1ph2@outlook.com>
2025-02-12 00:48:17 +0800

    Support setup.sh for msys2

--- setup.sh
+++ setup.sh
@@ -1,19 +1,66 @@
 #! /usr/bin/env bash
 
+# set -o xtrace
+
+arch_setup() {
+    sudo pacman -S --needed \
+         ctags global \
+         python python-pygments \
+         ttf-ubuntu-mono-nerd wqy-microhei ttf-hanazono \
+         clang pyright ruff
+
+    # setup python venv for lsp bridge
+    lsp_bridge_pyenv_path=~/.emacs.d/lsp.bridge.pyenv
+    if [[ -d ${lsp_bridge_pyenv_path} ]]; then
+        return
+    fi
+    python -m venv ${lsp_bridge_pyenv_path}
+    source ${lsp_bridge_pyenv_path}/bin/activate
+    python -m pip install --upgrade pip
+    pip install epc orjson sexpdata six setuptools paramiko rapidfuzz watchdog packaging
+}
+
+msys_setup() {
+    pacman -S --needed \
+           ucrt64/mingw-w64-ucrt-x86_64-ctags \
+           ucrt64/mingw-w64-ucrt-x86_64-global \
+           ucrt64/mingw-w64-ucrt-x86_64-ttf-ubuntu-mono-nerd \
+           ucrt64/mingw-w64-ucrt-x86_64-clang \
+           ucrt64/mingw-w64-ucrt-x86_64-python \
+           ucrt64/mingw-w64-ucrt-x86_64-python-pygments \
+           ucrt64/mingw-w64-ucrt-x86_64-python-ruff-lsp \
+           ucrt64/mingw-w64-ucrt-x86_64-python-six \
+           ucrt64/mingw-w64-ucrt-x86_64-python-setuptools \
+           ucrt64/mingw-w64-ucrt-x86_64-python-paramiko \
+           ucrt64/mingw-w64-ucrt-x86_64-python-rapidfuzz \
+           ucrt64/mingw-w64-ucrt-x86_64-python-watchdog \
+           ucrt64/mingw-w64-ucrt-x86_64-python-packaging
+
+    # setup python venv for lsp bridge
+    lsp_bridge_pyenv_path=~/.emacs.d/lsp.bridge.pyenv
+    if [[ -d ${lsp_bridge_pyenv_path} ]]; then
+        return
+    fi
+    python -m venv --system-site-packages ${lsp_bridge_pyenv_path}
+    source ${lsp_bridge_pyenv_path}/bin/activate
+    python -m pip install --upgrade pip
+    pip install epc orjson sexpdata
+}
+
+echo "Install dependencys(we may need sudo rights)..."
 # install dependencys
 os=$(cat /etc/os-release | grep '^NAME=')
-if [[ ${os} != "NAME=\"Arch Linux\"" ]]; then
-    echo "we only support Arch linux currently."
+if [[ "${os}" == "NAME=\"Arch Linux\"" ]]; then
+    arch_setup
+elif [[ "${os}" == "NAME=MSYS2" ]]; then
+    msys_setup
+else
+    echo "${os} is not supported currently."
     exit 1
 fi
+echo "Done!"
 
-sudo pacman -S --needed \
-     git \
-     ctags global \
-     python python-pygments \
-     ttf-ubuntu-mono-nerd wqy-microhei ttf-hanazono \
-     clang pyright ruff
-
+echo "Install 3rd-party fonts..."
 # install sarasaTermSCNerd
 fc-list | grep -i "Sarasa Term SC Nerd"
 if [[ $? -ne 0 ]]; then
@@ -27,13 +74,4 @@ if [[ $? -ne 0 ]]; then
     sudo fc-cache
     rm ./SarasaTermSCNerd.ttc.tar.gz
 fi
-
-# setup python venv for lsp bridge
-lsp_bridge_pyenv_path=~/.emacs.d/lsp.bridge.pyenv
-if [[ -d ${lsp_bridge_pyenv_path} ]]; then
-    exit 0
-fi
-python -m venv ${lsp_bridge_pyenv_path}
-source ${lsp_bridge_pyenv_path}/bin/activate
-python -m pip install --upgrade pip
-pip install epc orjson sexpdata six setuptools paramiko rapidfuzz watchdog packaging
+echo "Done!"
